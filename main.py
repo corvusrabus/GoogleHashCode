1131fd5bcbc15eb08933669679e5e88d153f2d42
Adrien Vakili <git@vaki.li>
2021-01-18 22:31:49 +0000

    Read only first line

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 path = 'data/a_example'
 
 def parse_first_line(file):
-    first_line = file.readlines()[0]
+    first_line = file.readline()
     M, T2, T3, T4 = first_line.split( )
     return M, T2, T3, T4
 
+ 
 
 if __name__ == '__main__':
     first_line = list(map(int,input().split()))
